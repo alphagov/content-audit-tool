219e54ed389aa43964727add9adf57ba119e2b5b
Pablo Manrubia <pmanrubia@gmail.com>
2018-06-28 13:55:02 +0100

    Revert "Revert "Remove left outer join from Finding unallocated items""

--- app/domain/policies/unallocated.rb
+++ app/domain/policies/unallocated.rb
@@ -1,7 +1,7 @@
 module Policies
   class Unallocated
     def self.call(scope, allocated_to: nil) # rubocop:disable Lint/UnusedMethodArgument
-      scope.left_joins(:allocation).where(allocations: { id: nil })
+      scope.where('content_items.content_id not in (select content_id from allocations)')
     end
   end
 end
