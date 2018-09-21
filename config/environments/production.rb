c87e89b2996670ffbe0d67caa6adc40ba2ba3267
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-09-21 12:52:17 +0100

    Remove custom logstasher config
    This is handled by govuk_app_config now.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.logstasher.enabled = true
-  config.logstasher.logger = Logger.new(Rails.root.join("log/production.json.log"))
-  config.logstasher.suppress_app_log = true
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -52,7 +49,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
